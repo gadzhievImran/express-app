9276d70a9ad0783c71be70b1ca9b6373fa38777f
Гаджиев Имран <i.gadzhiev@aoamt.ru>
2019-09-18 13:24:14 +0300

    update

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const path = require('path');
+const { highlight } = require('highlightjs');
+const moment = require('moment');
+require('moment/locale/ru');
+moment.locale('ru');
+
 const snippets = require('./data/snippets.json');
 
 const server = express();
@@ -6,6 +12,8 @@ const server = express();
 server.set('view engine', 'pug');
 
 server.use(express.static('public'));
+server.use('/lib', express.static('node_modules'));
+
 server.use(express.urlencoded({ extended: true }));
 
 server.get('/', (req, res) => {
@@ -14,7 +22,9 @@ server.get('/', (req, res) => {
 
 server.get('/snippets', (req, res) => {
     res.render('list', {
-        snippets
+        title: 'Сниппеты',
+        snippets,
+        moment
     });
 });
 
@@ -22,6 +32,17 @@ server.get('/snippets/:snippetId', (req, res) => {
     const snippet = snippets.find(s => s.id === req.params.snippetId);
 
     res.render('view', {
+        title: snippet.filename,
+        snippet,
+        highlight,
+        moment
+    })
+});
+
+server.get('/snippets/:snippetId/update', (res, req) => {
+    const snippet = snippets.find(s => s.id === req.params.snippetId);
+
+    res.render('form', {
         title: snippet.filename,
         snippet
     })
