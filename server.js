7ded41591615a190e0f5a1b6e20324b66cb2cd98
Гаджиев Имран <i.gadzhiev@aoamt.ru>
2019-09-18 12:09:58 +0300

    update

--- server.js
+++ server.js
@@ -18,4 +18,13 @@ server.get('/snippets', (req, res) => {
     });
 });
 
+server.get('/snippets/:snippetId', (req, res) => {
+    const snippet = snippets.find(s => s.id === req.params.snippetId);
+
+    res.render('view', {
+        title: snippet.filename,
+        snippet
+    })
+});
+
 server.listen(3000, () => console.log('Сервер запущен по адресу http://localhost:3000'));
